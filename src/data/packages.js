8e2ac054389968efa235e8c2e0cb1d2e112dd6e9
kratje050 <45762580+kratje050@users.noreply.github.com>
2026-07-08 16:30:37 +0200

    Add package data

---
+++ src/data/packages.js
@@ -0,0 +1,45 @@
+export const packages = [
+  {
+    name: "Portretshoot",
+    price: "€80",
+    details: "Inclusief 3 digitale beelden",
+    shoot: "Portretshoot",
+  },
+  {
+    name: "Cakesmash",
+    price: "€150",
+    details: "Inclusief 7 digitale beelden + taart",
+    shoot: "Cakesmash",
+    featured: true,
+  },
+  {
+    name: "Zwangerschapsshoot",
+    price: "€200",
+    details: "Inclusief 7 digitale beelden",
+    shoot: "Zwangerschapsshoot",
+  },
+  {
+    name: "Gezinsshoot",
+    price: "€200",
+    details: "Inclusief 7 digitale beelden",
+    shoot: "Gezinsshoot",
+  },
+  {
+    name: "Newbornshoot",
+    price: "€175",
+    details: "Inclusief 7 digitale beelden",
+    shoot: "Newbornshoot",
+  },
+  {
+    name: "Extra beeld",
+    price: "€7,50",
+    details: "Per stuk bij te bestellen uit jouw online galerij",
+    shoot: "Anders",
+  },
+  {
+    name: "Reiskosten",
+    price: "€0,35",
+    details: "Per kilometer buiten de afgesproken omgeving",
+    shoot: "Anders",
+  },
+];
